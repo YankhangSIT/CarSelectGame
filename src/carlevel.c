85f0b28c057b93ecc1855aeaa47a0924503bf66e
YankhangSIT <112969742+YankhangSIT@users.noreply.github.com>
2022-09-25 16:11:56 +0800

    FINISHED EVERYTHING BUT HARDCODED.

--- src/carlevel.c
+++ src/carlevel.c
@@ -1,8 +1,10 @@
-
 #include "cprocessing.h"
 #include "stdio.h"
 #include "utils.h"
 #include "mainmenu.h"
+#include "math.h"
+
+#define PI (3.141592653589793)
 
 struct Car {
 	CP_Vector Pos;
@@ -10,6 +12,8 @@ struct Car {
 	int Selected;
 	float Direction;
 };
+
+float speed = 200.0;
 
 int carArray[3];
 
@@ -43,17 +47,12 @@ void Car_Level_Init()
 	//carArray[0] = redCar;
 	//carArray[1] = greenCar;
 	//carArray[2] = blueCar;
+	
 }
 
 void Car_Level_Update()
 {
 	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-
-	/* 
-	if (CP_Input_MouseClicked()) {
-		printf("%f, %f\n", CP_Input_GetMouseX(), CP_Input_GetMouseY());
-	}
-	*/
 
 	//draw circles
 	CP_Settings_Fill(redCar.Color);
@@ -89,65 +88,120 @@ void Car_Level_Update()
 			redCar.Selected = 1;
 			greenCar.Selected = 0;
 			blueCar.Selected = 0;
-			printf("redcar selected\n");
+			//printf("redcar selected\n");
 		}
 		else if (IsCircleClicked(greenCar.Pos.x, greenCar.Pos.y, 70, mouseClickPos.x, mouseClickPos.y) == 1) {
 			redCar.Selected = 0;
 			greenCar.Selected = 1;
 			blueCar.Selected = 0;
-			printf("greencar selected\n");
+			//printf("greencar selected\n");
 		}
 		else if (IsCircleClicked(blueCar.Pos.x, blueCar.Pos.y, 70, mouseClickPos.x, mouseClickPos.y) == 1) {
 			redCar.Selected = 0;
 			greenCar.Selected = 0;
 			blueCar.Selected = 1;
-			printf("bluecar selected\n");
+			//printf("bluecar selected\n");
 		}
 	}
 
 	// if car is selected, A and D will turn it.
 	if (redCar.Selected == 1) {
 		if (CP_Input_KeyDown(KEY_A)) {
-			redCar.Direction -= 1.f;
+			if (redCar.Direction > 0) {
+				redCar.Direction -= 1.f;
+			}
+			else {
+				redCar.Direction = 359;
+			}
+			//printf("redcar angle is: %f\n", redCar.Direction);
 		}
 		else if (CP_Input_KeyDown(KEY_D)) {
-			redCar.Direction += 1.f;
-		}
-		else if (CP_Input_KeyDown(KEY_W)) {
-			redCar.Pos.x += 10;
+			if (redCar.Direction < 359) {
+				redCar.Direction += 1.f;
+			}
+			else {
+				redCar.Direction = 0;
+			}
+			//printf("redcar angle is: %f\n", redCar.Direction);
 		}
 	}
 	else if (greenCar.Selected == 1) {
 		if (CP_Input_KeyDown(KEY_A)) {
-			greenCar.Direction -= 1.f;
+			if (greenCar.Direction > 0) {
+				greenCar.Direction -= 1.f;
+			}
+			else {
+				greenCar.Direction = 359;
+			}
+			//printf("greencar angle is: %f\n", greenCar.Direction);
 		}
 		else if (CP_Input_KeyDown(KEY_D)) {
-			greenCar.Direction += 1.f;
+			if (greenCar.Direction < 359) {
+				greenCar.Direction += 1.f;
+			}
+			else {
+				greenCar.Direction = 0;
+			}
+			//printf("greencar angle is: %f\n", greenCar.Direction);
 		}
 	}
 	else if (blueCar.Selected == 1) {
 		if (CP_Input_KeyDown(KEY_A)) {
-			blueCar.Direction -= 1.f;
+			if (blueCar.Direction > 0) {
+				blueCar.Direction -= 1.f;
+			}
+			else {
+				blueCar.Direction = 359;
+			}
+			//printf("bluecar angle is: %f\n", blueCar.Direction);
 		}
 		else if (CP_Input_KeyDown(KEY_D)) {
-			blueCar.Direction += 1.f;
+			if (blueCar.Direction < 359) {
+				blueCar.Direction += 1.f;
+			}
+			else {
+				blueCar.Direction = 0;
+			}
+			//printf("bluecar angle is: %f\n", blueCar.Direction);
 		}
 	}
 
+	// if car is selected, W and S will move it forward or back
 
-	// movement for circle using WASD
-	CP_Vector dir = CP_Vector_Set(0.f, 0.f);
-	if (CP_Input_KeyTriggered(KEY_W)) {
-		dir.y += 1.f;
+	float dtSpeed = speed * CP_System_GetDt(); //x units per second
+
+	if (redCar.Selected == 1) {
+		//convert redcar direction (angle) to a vector (radians)
+		CP_Vector direction = AngleToVector(redCar.Direction * (PI / 180.0));
+		CP_Vector norm = CP_Vector_Normalize(direction);
+		if (CP_Input_KeyDown(KEY_W)) {
+			redCar.Pos = CP_Vector_Add(redCar.Pos, CP_Vector_Scale(norm, dtSpeed));
+		}
+		else if (CP_Input_KeyDown(KEY_S)) {
+			redCar.Pos = CP_Vector_Subtract(redCar.Pos, CP_Vector_Scale(norm, dtSpeed));
+		}
 	}
-	else if (CP_Input_KeyTriggered(KEY_A)) {
-		dir.x -= 1.f;
+	else if (greenCar.Selected == 1) {
+		//convert redcar direction (angle) to a vector (radians)
+		CP_Vector direction = AngleToVector(greenCar.Direction * (PI / 180.0));
+		CP_Vector norm = CP_Vector_Normalize(direction);
+		if (CP_Input_KeyDown(KEY_W)) {
+			greenCar.Pos = CP_Vector_Add(greenCar.Pos, CP_Vector_Scale(norm, dtSpeed));
+		}
+		else if (CP_Input_KeyDown(KEY_S)) {
+			greenCar.Pos = CP_Vector_Subtract(greenCar.Pos, CP_Vector_Scale(norm, dtSpeed));
+		}
 	}
-	else if (CP_Input_KeyTriggered(KEY_S)) {
-		dir.y -= 1.f;
-	}
-	else if (CP_Input_KeyTriggered(KEY_D)) {
-		dir.x += 1.f;
+	else if (blueCar.Selected == 1) {
+		//convert redcar direction (angle) to a vector (radians)
+		CP_Vector direction = AngleToVector(blueCar.Direction * (PI / 180.0));
+		CP_Vector norm = CP_Vector_Normalize(direction);
+		if (CP_Input_KeyDown(KEY_W)) {
+			blueCar.Pos = CP_Vector_Add(blueCar.Pos, CP_Vector_Scale(norm, dtSpeed));
+		}
+		else if (CP_Input_KeyDown(KEY_S)) {
+			blueCar.Pos = CP_Vector_Subtract(blueCar.Pos, CP_Vector_Scale(norm, dtSpeed));
+		}
 	}
 }
 
